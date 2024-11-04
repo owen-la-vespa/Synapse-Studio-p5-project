0b7dd45a737f3a6425e1e37d9bcc649a4bcd3777
owen-la-vespa <55978738+owen-la-vespa@users.noreply.github.com>
2024-11-04 12:40:49 +0000

    added a random walk program

--- sketch.js
+++ sketch.js
@@ -1,7 +1,29 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(0);
+  colorMode(RGB)
 }
 
 function draw() {
-  background(220);
+  stroke(random(255));
+  strokeWeight(1);
+
+  // Random walk for a single particle
+  let x = width / 2;
+  let y = height / 2;
+
+  let i = 0;
+  function step() {
+    if (i < 100) {
+      let stepSize = random(1, 50);
+      let angle = random(TWO_PI);
+      x += stepSize * cos(angle);
+      y += stepSize * sin(angle);
+      point(x, y);
+      i++;
+      setTimeout(step, 10); // Delay of 10 milliseconds
+    }
+  }
+
+  step();
 }

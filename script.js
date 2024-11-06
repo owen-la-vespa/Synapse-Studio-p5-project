1292579210e5792786c01b672843534e806da78d
Faraan <faraanqa@gmail.com>
2024-11-06 07:44:13 +0000

    added motion capture to the hand, only 1 hand is currently being tracked

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-let capture;
+let handPose;
+let video;
+let hands = [];
 
 function setup() {
-  createCanvas(640, 480); // Increase canvas size
-  capture = createCapture(VIDEO);
-  capture.size(640, 480); // Increase video size
-  capture.hide(); // Hide the extra video element
+  createCanvas(640, 480);
+  
+  // Create the webcam video and hide it
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
+
+  // Initialize handPose model with the video feed
+  handPose = ml5.handpose(video, modelReady);
+
+  // Listen for the "predict" event to get hand data
+  handPose.on('predict', gotHands);
+}
+
+function modelReady() {
+  console.log("Handpose model loaded!");
 }
 
 function draw() {
-  background(255);
-  if (capture.loadedmetadata) { // Ensure the video has loaded
-      image(capture, 0, 0, 640, 480);
+  // Draw the webcam video
+  image(video, 0, 0, width, height);
+
+  // Draw all the tracked hand points
+  for (let i = 0; i < hands.length; i++) {
+    let hand = hands[i];
+    for (let j = 0; j < hand.landmarks.length; j++) {
+      let keypoint = hand.landmarks[j];
+      fill(0, 255, 0);
+      noStroke();
+      circle(keypoint[0], keypoint[1], 10);
+    }
   }
+}
+
+// Callback function for when handPose outputs data
+function gotHands(results) {
+  hands = results; // Save the hand data to the global variable
 }

df43df13b8102d34dd2c8ee9a551d59c61eae99f
owen-la-vespa <55978738+owen-la-vespa@users.noreply.github.com>
2024-11-08 15:59:29 +0000

    added pose

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 let handPose;
+let bodyPose;
+let connections;
+let poses = [];
 let video;
 let hands = [];
 
 function preload() {
-  // Load the handPose model
+  // Load the Pose models
   handPose = ml5.handPose();
+  bodyPose = ml5.bodyPose();
 }
 
 function setup() {
@@ -15,6 +19,10 @@ function setup() {
   video.hide();
   // start detecting hands from the webcam video
   handPose.detectStart(video, gotHands);
+  // Start detecting poses in the webcam video
+  bodyPose.detectStart(video, gotPoses);
+  // Get the skeleton connection information
+  connections = bodyPose.getSkeleton();
 }
 
 function draw() {
@@ -28,13 +36,52 @@ function draw() {
       let keypoint = hand.keypoints[j];
       fill(0, 255, 0);
       noStroke();
-      circle(keypoint.x, keypoint.y, 10);
+      circle(keypoint.x, keypoint.y, 5);
+    }
+  }
+
+  // Draw the skeleton connections
+  for (let i = 0; i < poses.length; i++) {
+    let pose = poses[i];
+    for (let j = 0; j < connections.length; j++) {
+      let pointAIndex = connections[j][0];
+      let pointBIndex = connections[j][1];
+      let pointA = pose.keypoints[pointAIndex];
+      let pointB = pose.keypoints[pointBIndex];
+      // Only draw a line if we have confidence in both points
+      if (pointA.confidence > 0.1 && pointB.confidence > 0.1) {
+        stroke(255, 0, 0);
+        strokeWeight(2);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+      }
+    }
+  }
+
+  // Iterate through all the poses
+  for (let i = 0; i < poses.length; i++) {
+    let pose = poses[i];
+    // Iterate through all the keypoints for each pose
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      let keypoint = pose.keypoints[j];
+      // Only draw a circle if the keypoint's confidence is greater than 0.1
+      if (keypoint.confidence > 0.1) {
+        fill(0, 255, 0);
+        noStroke();
+        circle(keypoint.x, keypoint.y, 10);
+      }
     }
   }
 }
+
 
 // Callback function for when handPose outputs data
 function gotHands(results) {
   // save the output to the hands variable
   hands = results;
+}
+
+// Callback function for when the model returns pose data
+function gotPoses(results) {
+  // Store the model's results in a global variable
+  poses = results;
 }
